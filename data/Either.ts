d5b54b753b520695ef4be9ac96b3344acefad965
Steven Yap <stevenyap@futureworkz.com>
2024-04-15 13:29:15 +0800

    Add Either decoding

--- data/Either.ts
+++ data/Either.ts
@@ -1,6 +1,5 @@
 import * as JD from "decoders"
 import type { DecodeResult } from "decoders"
-import type { Annotation } from "decoders/annotate"
 
 export type Either<E, T> = Left<E> | Right<T>
 export type Left<E> = { _t: "Left"; error: E }
@@ -41,27 +40,75 @@ export function fromLeft<E, T>(result: Either<E, T>): E | null {
   return result._t === "Left" ? result.error : null
 }
 
+/** Annotation type is not exported from decoders package
+ * hence, we have to do a sleight of hand to trick it out
+ */
 export function fromDecodeResult<T>(
   result: DecodeResult<T>,
-): Either<Annotation, T> {
+): Either<Exclude<typeof result.error, undefined>, T> {
   return result.ok ? right(result.value) : left(result.error)
 }
 
-export function eitherDecoder<E, T>(
-  leftDecoder: JD.Decoder<E>,
-  rightDecoder: JD.Decoder<T>,
-): JD.Decoder<Either<E, T>> {
+/** Type inference for a JD.object({ t: JD.Decoder<T> }) is always { t: T | undefined }
+ * hence, we have to do a roundabout way to decode an Either type
+ * Ref: https://github.com/nvie/decoders/issues/930
+ */
+export function leftDecoder<E>(
+  errorDecoder: JD.Decoder<E>,
+): JD.Decoder<Left<E>> {
   return JD.define((blob, ok, err) => {
-    const result = rightDecoder.decode(blob)
-    if (result.ok === true) {
-      return ok(right(result.value))
-    } else {
-      const errorResult = leftDecoder.decode(blob)
-      if (errorResult.ok === true) {
-        return ok(left(errorResult.value))
-      } else {
-        return err("Invalid Either type")
-      }
+    const result = JD.object({
+      _t: JD.constant("Left"),
+      error: JD.unknown,
+    }).decode(blob)
+    if (result.ok === false) {
+      return err(result.error)
+    }
+
+    const errorM = errorDecoder.decode(result.value.error)
+    if (errorM.ok === false) {
+      return err(errorM.error)
+    }
+
+    return ok({ _t: "Left", error: errorM.value })
+  })
+}
+
+/** Type inference for a JD.object({ t: JD.Decoder<T> }) is always { t: T | undefined }
+ * hence, we have to do a roundabout way to decode an Either type
+ * Ref: https://github.com/nvie/decoders/issues/930
+ */
+export function rightDecoder<T>(
+  valueDecoder: JD.Decoder<T>,
+): JD.Decoder<Right<T>> {
+  return JD.define((blob, ok, err) => {
+    const result = JD.object({
+      _t: JD.constant("Right"),
+      value: JD.unknown,
+    }).decode(blob)
+    if (result.ok === false) {
+      return err(result.error)
+    }
+
+    const valueM = valueDecoder.decode(result.value.value)
+    if (valueM.ok === false) {
+      return err(valueM.error)
+    }
+
+    return ok({ _t: "Right", value: valueM.value })
+  })
+}
+
+export function eitherDecoder<E, T>(
+  errorDecoder: JD.Decoder<E>,
+  valueDecoder: JD.Decoder<T>,
+): JD.Decoder<Either<E, T>> {
+  return JD.select(JD.object({ _t: JD.oneOf(["Left", "Right"]) }), ({ _t }) => {
+    switch (_t) {
+      case "Left":
+        return leftDecoder(errorDecoder)
+      case "Right":
+        return rightDecoder(valueDecoder)
     }
   })
 }
