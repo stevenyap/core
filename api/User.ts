4db9476999272f8ed9b86b43e3ad3aae1986ee37
Iker <tdtrinh.web@gmail.com>
2024-06-03 17:58:21 +0700

    Add user api

---
+++ api/User.ts
@@ -0,0 +1,27 @@
+import * as JD from "decoders"
+import { User, userDecoder } from "../app/User"
+import { authResponseDecoder, AuthGetApi } from "../data/Api"
+import { numberFromStringDecoder } from "../data/Decoder"
+import { PositiveInt, positiveIntDecoder } from "../data/PositiveInt"
+
+export type Users = AuthGetApi<"/users/:userID", UrlParams, ErrorCode, Payload>
+
+export type UrlParams = {
+  userID: PositiveInt
+}
+
+export type Payload = User
+
+export type ErrorCode = "USER_NOT_FOUND"
+
+export const contract: Users = {
+  method: "GET",
+  route: "/users/:userID",
+  urlDecoder: JD.object({
+    userID: numberFromStringDecoder.transform(positiveIntDecoder.verify),
+  }),
+  responseDecoder: authResponseDecoder(
+    JD.oneOf(["USER_NOT_FOUND"]),
+    userDecoder,
+  ),
+}
